5add51cd94142170910d2536d0d60e25027245b7
danigm <dani@danigm.net>
2009-08-10 19:27:12 +0200

    nuevo script para publicar

---
+++ scripts/ALL.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+./imagen.sh ../$1.png
+scp ../vinieta.png ../$1.png root@danigm.net:/var/www/tira/
+
+pub="ftp://linuxhispano.net/public_html/tira/"
+USER="linuxhis"
+read -s -p "linuxhis - Password:" PASS
+lftp -u $USER,$PASS $pub -e "put ../vinieta.png ../$1.png; exit"
+
+cat drupal.html | sed "s/IMG/$1/g"
+
+
+
