6fb04812d9e1c344b6bb0f35c1be349cd2514b28
Daniel García Moreno <danigm@wadobo.com>
2017-05-17 09:59:10 +0200

    Quitando compresión de imágenes

--- scripts/imagen.sh
+++ scripts/imagen.sh
@@ -10,10 +10,10 @@ convert $1 -crop 227x227+1+1 vinieta.png
 #convert vinieta.png -fill white -fuzz 15% -draw 'color 1,1 replace' vinieta.png
 #y esto redimensiona la vinieta y le asigna la profundidad de color
 convert vinieta.png -resize 170x171 vinieta.png
-convert vinieta.png -colors 255 -verbose +dither -depth 8 vinieta.png
+#convert vinieta.png -colors 255 -verbose +dither -depth 8 vinieta.png
 echo "imagen vinieta.png creada"
 cp -f vinieta.png ../vinieta.png
 
 #luego se asigna la profundidad a la imagen principal
-convert $1 -colors 255 -verbose +dither -depth 8 $1
+#convert $1 -colors 255 -verbose +dither -depth 8 $1
 echo "imagen $1 modificada"
