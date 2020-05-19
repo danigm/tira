4d029abc4bc8feb02f617801cadc751bf2b5809f
Daniel García Moreno <dani@danigm.net>
2020-05-19 18:41:50 +0200

    perdon

--- scripts/ftp.sh
+++ scripts/ftp.sh
@@ -3,4 +3,4 @@
 FTP_DOMAIN=linuxhispano.net
 source .ENV
 
-toolbox run -c fedora-toolbox-30 lftp -u $FTP_USER,$FTP_PASS -e "cd tira; put $1; bye" $FTP_DOMAIN
+lftp -u $FTP_USER,$FTP_PASS -e "cd tira; put $1; bye" $FTP_DOMAIN
