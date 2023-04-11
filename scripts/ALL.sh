f11db4940250d3afc36c6fff3040abb4f3820b08
Daniel García Moreno <dani@danigm.net>
2023-04-11 19:07:08 +0200

    editor

--- scripts/ALL.sh
+++ scripts/ALL.sh
@@ -4,7 +4,6 @@
 # ./ftp.sh ../$1.png
 ./ftp.sh ../vinieta.png
 
-cat wordpress.html | sed "s/IMG/$1/g"
+cat wordpress.html | sed "s/IMG/$1/g" | sed "s/YEAR/$(date +%Y)/g" | sed "s/MONTH/$(date +%m)/g"
 
 echo "$1.png" >> ../lista.txt
-
