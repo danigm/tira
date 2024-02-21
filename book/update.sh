a53d77e21ec842b7173afcce2556aab3f5340a17
Daniel Garcia Moreno <daniel.garcia@suse.com>
2024-02-21 18:33:37 +0100

    book: Arreglado script de update

--- book/update.sh
+++ book/update.sh
@@ -10,8 +10,8 @@ last=$(ls all/ | tail -1 | sed 's/.rst//')
 last_img=$(echo $last | sed 's/[0-9]\+-//').png
 prev=$(ls all/ | tail -2 | head -1 | sed 's/.rst//')
 
-lftp -e "put -O www/tira/all _build/html/all/$last.html" -u $HOME_FTP_USER,$HOME_FTP_PASS $HOME_DOMAIN
-lftp -e "put -O www/tira/_images _build/html/_images/$last_img" -u $HOME_FTP_USER,$HOME_FTP_PASS $HOME_DOMAIN
+lftp -e "put -O www/tira/all _build/html/all/$last.html; bye" -u $HOME_FTP_USER,$HOME_FTP_PASS $HOME_DOMAIN
+lftp -e "put -O www/tira/_images _build/html/_images/$last_img; bye" -u $HOME_FTP_USER,$HOME_FTP_PASS $HOME_DOMAIN
 
-lftp -e "put -O www/tira/all _build/html/all/$prev.html" -u $HOME_FTP_USER,$HOME_FTP_PASS $HOME_DOMAIN
-lftp -e "put -O www/tira _build/html/index.html" -u $HOME_FTP_USER,$HOME_FTP_PASS $HOME_DOMAIN
+lftp -e "put -O www/tira/all _build/html/all/$prev.html; bye" -u $HOME_FTP_USER,$HOME_FTP_PASS $HOME_DOMAIN
+lftp -e "put -O www/tira _build/html/index.html; bye" -u $HOME_FTP_USER,$HOME_FTP_PASS $HOME_DOMAIN
