aad5738163d0b4bd5cd06b28bd6ce8a674f58e2e
Daniel Garcia Moreno <daniel.garcia@suse.com>
2024-12-19 08:38:09 +0100

    politica-seguridad

--- scripts/imagen.sh
+++ scripts/imagen.sh
@@ -5,11 +5,11 @@
 
 #esto recorta la primera vinieta
 #convert $1 -crop 224x226+0+4 vinieta.png
-convert $1 -crop 227x227+1+1 vinieta.png
+magick convert $1 -crop 227x227+1+1 vinieta.png
 #esto rellena las esquinas de blanco
 #convert vinieta.png -fill white -fuzz 15% -draw 'color 1,1 replace' vinieta.png
 #y esto redimensiona la vinieta y le asigna la profundidad de color
-convert vinieta.png -resize 170x171 vinieta.png
+magick convert vinieta.png -resize 170x171 vinieta.png
 #convert vinieta.png -colors 255 -verbose +dither -depth 8 vinieta.png
 echo "imagen vinieta.png creada"
 cp -f vinieta.png ../vinieta.png
