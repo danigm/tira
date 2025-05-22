8b7b6baebc6762baea0068f6d6fa820b1b27ef1f
Daniel Garcia Moreno <daniel.garcia@suse.com>
2025-05-22 14:27:37 +0200

    offtopic

--- scripts/ftp.sh
+++ scripts/ftp.sh
@@ -4,5 +4,5 @@ FTP_DOMAIN=linuxhispano.net
 HOME_DOMAIN=ftp.cluster029.hosting.ovh.net
 source .ENV
 
-lftp -u $FTP_USER,$FTP_PASS -e "cd tira; put $1; bye" $FTP_DOMAIN
+#lftp -u $FTP_USER,$FTP_PASS -e "cd tira; put $1; bye" $FTP_DOMAIN
 lftp -u $HOME_FTP_USER,$HOME_FTP_PASS -e "cd www/tira; put $1; cd ../blog; put $1; bye" $HOME_DOMAIN
